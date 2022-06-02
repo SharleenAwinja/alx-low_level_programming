0c050d8e7dc8a1c1a1ff70be4bf688f43776e13d
SharleenAwinja <awinjasharleen@gmail.com>
2022-06-02 13:55:48 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-int main(void)
-{
 /**
- * main - Entry point
- * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19':
+ * main - my entry point
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
  * Return: Always (1)
  */
-printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
