dbad648c190ae0cfce9ceb59e1a420a76cf02cfc
SharleenAwinja <awinjasharleen@gmail.com>
2022-06-02 13:30:38 -0700

    Print

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@ int main(void)
  * 
  * Return: Always (1)
  */
-write("and that piece pf art is useful\" - Dora Korpar, 2015-10-19\n");
+printf("and that piece pf art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
