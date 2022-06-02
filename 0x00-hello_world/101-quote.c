ee9c982b3de34ee04a1546a35085cf9301e31f0d
SharleenAwinja <awinjasharleen@gmail.com>
2022-06-02 13:32:53 -0700

    Correct printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@ int main(void)
  * 
  * Return: Always (1)
  */
-printf("and that piece pf art is useful\" - Dora Korpar, 2015-10-19\n");
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
