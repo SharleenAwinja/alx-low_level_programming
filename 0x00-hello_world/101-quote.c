1c7ad01ea57ab23d566ea41b22e28a370ba73e9f
SharleenAwinja <awinjasharleen@gmail.com>
2022-06-02 13:28:57 -0700

    Write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always (1)
- */
+#include <unistd.h>
 int main(void)
 {
-	write(2,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-		59);
-	
-	return (1);
+/**
+ * main - Entry point
+ * 
+ * Return: Always (1)
+ */
+write("and that piece pf art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
 }
-
-
