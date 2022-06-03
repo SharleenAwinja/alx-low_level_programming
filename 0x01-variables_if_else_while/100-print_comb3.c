7aa1a9117f16db801b041c0622ae45c5d3817a7e
SharleenAwinja <awinjasharleen@gmail.com>
2022-06-03 13:05:07 -0700

    Correction

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,10 +9,9 @@ int main(void)
 {
 	int tens;
 	int ones;
-
-		for (tens = 0; tens <= 9; tens++)
-   		 {
-		 for (ones = 0; ones <= 9; ones++)
+	
+	for (tens = 0; tens <=9; tens++)
+	for (ones = 0; ones <= 9; ones++)
 	{
 	  putchar(tens + '0');
 	  putchar(ones + '0');
@@ -20,7 +19,7 @@ int main(void)
 	  if (!(tens == 9 && ones == 9))
 	    {
 	      putchar(',');
-	      putchar(' ');
+	      putchar(',');
 	    }
 	}
     }
