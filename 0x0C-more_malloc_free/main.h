bf90ae058050827743f0b0a38d9588c1b6a8ebbf
SharleenAwinja <awinjasharleen@gmail.com>
2022-06-30 04:40:26 -0700

    101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,3 +1,5 @@
+int _putchar(char c);
+int main(int argc, char *argv[])
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int *array_range(int min, int max);
 void *_calloc(unsigned int nmemb, unsigned int size);
